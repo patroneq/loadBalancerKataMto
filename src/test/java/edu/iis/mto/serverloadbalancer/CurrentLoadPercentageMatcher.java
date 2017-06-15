dca7d29a1bac6840a0f123916d835f5c8f4a556e
Patron <patroneq@gmail.com>
2017-06-15 21:28:12 +0200

    refactorization

--- src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -19,7 +19,14 @@ public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server>{
 
 	@Override
 	protected boolean matchesSafely(Server server) {
-		return expectedLoadPercentage == server.currentLoadPercentage || Math.abs(expectedLoadPercentage - server.currentLoadPercentage) < 0.01d;
+		return doublesAreEqual(expectedLoadPercentage, server.currentLoadPercentage);
 	}
 
+	private boolean doublesAreEqual(double d1, double d2) {
+		return d1 == d2 || Math.abs(d1 - d2) < 0.01d;
+	}
+	
+	public static CurrentLoadPercentageMatcher hasCurrentLoadOf(double expectedLoadPercentage) {
+		return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+	}
 }
