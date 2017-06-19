00b8d26751e54aaff0751f1d0e8167f3c428ab12
Patron <patroneq@gmail.com>
2017-06-19 14:08:45 +0200

    placing vm on less loaded server

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -14,7 +14,7 @@ public class Server {
 	private List<Vm> vms = new ArrayList<Vm>();
 	
 	public boolean contains(Vm theVm) {
-		return true;
+		return vms.contains(theVm);
 	}
 
 	public Server(int capacity) {
