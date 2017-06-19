589138c2ba2d1f2b9f89aeb416ff67f272796db3
Patron <patroneq@gmail.com>
2017-06-19 14:39:35 +0200

    overfitted vm cannot be located on server

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -1,24 +1,35 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
 		for (Vm vm : vms) {
 			Server lessLoadedServer = null;
-			addToLessLoadedServer(servers, vm, lessLoadedServer);
+			addToLessLoadedServer(servers, vm);
 		}
 	}
 
-	private void addToLessLoadedServer(Server[] servers, Vm vm, Server lessLoadedServer) {
+	private void addToLessLoadedServer(Server[] servers, Vm vm) {
+		List<Server> capableServers = new ArrayList<Server>();
 		for (Server server : servers) {
-			lessLoadedServer = findLessLoadedServer(lessLoadedServer, server);
+			if(server.canFit(vm)) {
+				capableServers.add(server);
+			}
 		}
-		lessLoadedServer.addVm(vm);
+		Server lessLoadedServer = findLessLoadedServer(capableServers);
+		if (lessLoadedServer != null)
+			lessLoadedServer.addVm(vm);
 	}
 
-	private Server findLessLoadedServer(Server lessLoadedServer, Server server) {
-		if(lessLoadedServer == null || server.currentLoadPercentage < lessLoadedServer.currentLoadPercentage) {
-			lessLoadedServer = server;
+	private Server findLessLoadedServer(List<Server> capableServers) {
+		Server lessLoadedServer = null;
+		for (Server server : capableServers) {
+			if(lessLoadedServer == null || server.currentLoadPercentage < lessLoadedServer.currentLoadPercentage) {
+				lessLoadedServer = server;
+			}
 		}
 		return lessLoadedServer;
 	}
