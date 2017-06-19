97bbf068d7a31d13c740146d74636e0d5449ba6a
Patron <patroneq@gmail.com>
2017-06-19 21:49:12 +0200

    filling server partially

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -5,9 +5,15 @@ import org.hamcrest.Matcher;
 public class Server {
 
 	public double currentLoadPercentage;
+	public int capacity;
 
 	public boolean contains(Vm theVm) {
 		return true;
 	}
 
+	public Server(int capacity) {
+		super();
+		this.capacity = capacity;
+	}
+	
 }
